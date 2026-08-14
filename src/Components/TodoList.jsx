494843ef6e6e57deca7c02176f37322af9dff63f
cccileje <cccileje@gmail.com>
2026-08-13 20:57:39 -0400

    Add heading functionality

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -7,6 +7,13 @@ const [todos, setTodos] = useState([]);
 const [headingInput, setHeadingInput] = useState('');
 const [listInputs, setListInputs] = useState({});
 
+const handleTodo = () => {
+    if (headingInput.trim() !== '') {
+        setTodos([...todos, { heading: headingInput, lists: [] }]);
+        setHeadingInput('');
+    }
+};
+
     return (
         <>
             <div className="todo-container">
@@ -16,12 +23,23 @@ const [listInputs, setListInputs] = useState({});
                         type="text"
                         className="heading-input"
                         placeholder="Enter heading"
-                        
+                        value={headingInput} // Store value of the input field to the state variable headinginput
+                        onChange={(e) => {setHeadingInput(e.target.value);}} // Add onChange event handler to update headingInput state
                     />
-                    <button className="add-list-button">Add Heading</button>
+                    {/*Button push triggers handleTodo function */}
+                    <button className="add-list-button" onClick={handleTodo}>Add Heading</button>
                 </div>
             </div>
             <div className="todo_main">
+
+                {todos.map((todo, index) => ( // Iterate over each todo item in the todos array
+                    <div key={index} className='todo-card'> 
+                        <div className='heading_todo'>
+                            <h3>{todo.heading}</h3> {/* Displaying heading of todo item */}
+                            <button className='delete-button-heading' onClick={() => handleDeleteTodo(index)}>Delete Heading </button>
+                        </div>
+                    </div>
+                ))}
 
             </div>
         </>
